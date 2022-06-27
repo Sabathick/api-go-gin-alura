b81328e8a236a720518bb0a0fdbdf3cb41861612
Sabathick <erickick13@gmail.com>
2022-06-27 18:47:38 -0300

    Finishing the course

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/Sabathick/api-go-gin-alura/models"
+	"github.com/Sabathick/api-go-gin-alura/database"
 	"github.com/Sabathick/api-go-gin-alura/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno{
-		{Nome: "Erick Martins", CPF: "00000000000", RG: "4444444444"},
-		{Nome: "Erick Pereira", CPF: "00000000001", RG: "4444444443"},
-	}
+	database.ConectaComBancoDeDados()
 	routes.HandleRequests()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,7 @@ func HandleRequests() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/:nome", controllers.Saudacao)
-	r.Run(":8000")
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.Run()
 }
